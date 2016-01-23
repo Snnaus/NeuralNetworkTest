6c3aeb0b4aa44ce32dba9549795bb21f88b1dfea
Glenn <nausleyg@gmail.com>
2016-01-23 12:28:17 -0500

    Fixed some bugs with the csv formatter; also saved some of the trained networks.

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ var fs = require('fs'),
 
 var converter = new Converter({}), MVPs = [{ Player: 'Stephen Curry', Age: 26 },
                                            { Player: "Kevin Durant", Age: 25 },
-                                           { Player: 'Lebron James', Age: 28 },
-                                           { Player: "Lebron James", Age: 27 },
+                                           { Player: 'LeBron James', Age: 28 },
+                                           { Player: "LeBron James", Age: 27 },
                                            { Player: "Derrick Rose", Age: 22 },
-                                           { Player: "Lebron James", Age: 25 },
-                                           { Player: "Lebron James", Age: 24 },
+                                           { Player: "LeBron James", Age: 25 },
+                                           { Player: "LeBron James", Age: 24 },
                                            { Player: "Kobe Bryant", Age: 29 },
                                            { Player: "Dirk Nowitzki", Age: 28 },
                                            { Player: "Steve Nash", Age: 31 }];
 
-var playerAr = [], net = new synaptic.Architect.Perceptron(17,5,1), rawAr = [];
+var playerAr = [], net = new synaptic.Architect.Perceptron(17,2,1), rawAr = [];
 var ignoredStats = ['Rk', 'Player', 'Pos', 'Age', 'Tm', 'MVP', 'FG' ,'FGA','3P','3PA','2P','2PA','2P'];
 
 converter.on('end_parsed', function(JSONarray){
@@ -47,7 +47,7 @@ var cleanArray = function(result, MVPs){
     }else{
       return true;
     }
-  }).filter(function(player){ return player.GS >= 65; }).map(function(player){
+}).filter(function(player){ return player.GS >= 60; }).map(function(player){
     /*console.log(MVPs.filter(function(player2){ return player2.Player === player.Player && player2.Age === player.Age; }));*/
     if(MVPs.filter(function(player2){ return player2.Player === player.Player && player2.Age === player.Age; }).length > 0){
       player.MVP = 1;
@@ -62,9 +62,9 @@ var cleanArray = function(result, MVPs){
 }
 
 var testPlayer = function(players, player){
-  players = players.filter(function(play){ return play.Player == player.Player && play.Age == player.Age; });
+  var temp_players = players.filter(function(play){ return play.Player == player.Player && play.Age == player.Age; });
 
-  return Object.keys(players[0]).filter(function(key){ return ignoredStats.indexOf(key) === -1; }).map(function(key){ return players[0][key]; });
+  return Object.keys(temp_players[0]).filter(function(key){ return ignoredStats.indexOf(key) === -1; }).map(function(key){ return temp_players[0][key]; });
 }
 
 var params = {
@@ -73,16 +73,25 @@ var params = {
 var player = {
   Player: "Kobe Bryant",
   Age: 31
-};
-var player2 = {
-  Player: 'Lebron James',
+}, player2 = {
+  Player: 'LeBron James',
   Age: 25
+}, player3 = {
+    Player: 'Al Jefferson',
+    Age: 30
 };
 
 setTimeout(function(){
   console.log(net.trainer.train(playerAr, params));
   console.log(net.activate(testPlayer(rawAr, player)));
-  fs.writeFile('mvp.json', net.toJSON());
+  fs.writeFile('mvp.json', JSON.stringify(net.toJSON()), function(err){
+      if(err){
+          console.log(err);
+      }else{
+          console.log("The File was saved");
+      }
+  });
   console.log(net.activate(testPlayer(rawAr, player2)));
+  console.log(net.activate(testPlayer(rawAr, player3)));
 
 }, 5000);
